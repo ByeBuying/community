9327fafbd3384c51ad3d87f31d5a117fb8af5f99
kgd7054 <kgd7054@gmail.com>
2023-02-25 02:31:42 +0900

    added: review crud 추가

---
+++ controller/review.go
@@ -0,0 +1,213 @@
+package controller
+
+import (
+	config "community/conf"
+	"community/model"
+	"community/protocol"
+	"github.com/gin-gonic/gin"
+	"go-common/klay/elog"
+	"net/http"
+)
+
+type Review struct {
+	ctl *Controller
+	cfg *config.Config
+
+	// DB
+	communityDB *model.CommunityDB
+}
+
+// NewReview 객체 생성
+func NewReview(h *Controller, rep *model.Repositories) *Review {
+	r := &Review{
+		ctl: h,
+		cfg: h.config,
+	}
+
+	if err := rep.Get(&r.communityDB); err != nil {
+		elog.Crit("newCommunity", "error", err)
+	}
+
+	return r
+}
+
+// GetPostList
+// @Summary 리뷰 전체 게시물 조회
+// @Description grade - 평점
+// @Tags review
+// @Accept json
+// @Produce json
+// @Success 200 {object} protocol.ReviewListResp
+// @Router /napi/v1/review/list [get]
+func (r *Review) GetPostList(c *gin.Context) {
+
+	var reviewInfoList []protocol.ReviewPost
+
+	err := r.communityDB.GetReviewList(&reviewInfoList)
+	if err != nil {
+		r.ctl.RespError(c, nil, http.StatusNotFound, err)
+		return
+	}
+
+	//성공 응답
+	r.ctl.RespOK(c, &protocol.ReviewListResp{
+		RespHeader: protocol.NewRespHeader(protocol.Success),
+		ReviewList: reviewInfoList,
+	})
+
+}
+
+// GetPostDetail
+// @Summary 리뷰 게시물 조회
+// @Description grade - 평점
+// @Tags review
+// @Accept json
+// @Produce json
+// @Success 200 {object} protocol.ReviewDetailResp
+// @Router /napi/v1/review/{id} [get]
+func (r *Review) GetPostDetail(c *gin.Context) {
+	id := c.Param("id")
+
+	var reviewInfo protocol.ReviewPost
+
+	err := r.communityDB.GetReviewDetail(id, &reviewInfo)
+	if err != nil {
+		r.ctl.RespError(c, nil, http.StatusNotFound, err)
+		return
+	}
+
+	r.ctl.RespOK(c, &protocol.ReviewDetailResp{
+		RespHeader:   protocol.NewRespHeader(protocol.Success),
+		ReviewDetail: reviewInfo,
+	})
+
+}
+
+// CreatePostInfo
+// @Summary 리뷰 게시물 생성
+// @Description grade - 평점
+// @Tags review
+// @Accept json
+// @Produce json
+// @Param requestBody body protocol.ReviewPostReq true "resposne body"
+// @Success 200 {object} protocol.ReviewPostCreateRes
+// @Router /napi/v1/review [post]
+func (r *Review) CreatePostInfo(c *gin.Context) {
+	req := protocol.ReviewPostReq{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		r.ctl.RespError(c, &req, http.StatusUnprocessableEntity, "ShouldBindJSON", err)
+		return
+	}
+
+	//TODO 유저 레디스 값 체크
+
+	// TODO 유효성 체크
+
+	// db insert
+	err := r.communityDB.CreateReviewPost(req)
+	if err != nil {
+		r.ctl.RespError(c, nil, http.StatusBadRequest, "CreateReviewPost", err.Error())
+		return
+	}
+
+	r.ctl.RespOK(c, &protocol.ReviewPostCreateRes{
+		RespHeader: protocol.NewRespHeader(protocol.Success),
+		Stat:       1,
+	})
+}
+
+// UpdatePostInfo
+// @Summary 리뷰 게시물 수정
+// @Description grade - 평점
+// @Tags review
+// @Accept json
+// @Produce json
+// @Param requestBody body protocol.ReviewPostReq true "resposne body"
+// @Success 200 {object} protocol.ReviewPostUpdateRes
+// @Router /napi/v1/review/{id} [put]
+func (r *Review) UpdatePostInfo(c *gin.Context) {
+	id := c.Param("id")
+
+	req := protocol.ReviewPostReq{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		r.ctl.RespError(c, &req, http.StatusUnprocessableEntity, "ShouldBindJSON", err)
+		return
+	}
+
+	// TODO 유저 레디스 값 체크
+
+	// TODO 유효성 체크
+
+	// db update
+	err := r.communityDB.UpdateReviewPost(id, req)
+	if err != nil {
+		r.ctl.RespError(c, nil, http.StatusBadRequest, "UpdateReviewPost", err.Error())
+		return
+	}
+
+	r.ctl.RespOK(c, &protocol.ReviewPostUpdateRes{
+		RespHeader: protocol.NewRespHeader(protocol.Success),
+		Stat:       1,
+	})
+
+}
+
+// DeletePostInfo
+// @Summary 리뷰 게시물 삭제
+// @Description grade - 평점
+// @Description soft delete
+// @Tags review
+// @Accept json
+// @Produce json
+// @Param requestBody body protocol.ReviewPostReq true "resposne body"
+// @Success 200 {object} protocol.ReviewPostDeleteRes
+// @Router /napi/v1/review/{id} [patch]
+func (r *Review) DeletePostInfo(c *gin.Context) {
+	id := c.Param("id")
+
+	req := protocol.ReviewPostReq{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		r.ctl.RespError(c, &req, http.StatusUnprocessableEntity, "ShouldBindJSON", err)
+		return
+	}
+
+	// TODO 유저 레디스 값 체크
+
+	// TODO 유효성 체크
+
+	// db delete
+	err := r.communityDB.DeleteReviewPost(id)
+	if err != nil {
+		r.ctl.RespError(c, nil, http.StatusBadRequest, "UpdateReviewPost", err.Error())
+		return
+	}
+
+	r.ctl.RespOK(c, &protocol.ReviewPostDeleteRes{
+		RespHeader: protocol.NewRespHeader(protocol.Success),
+		Stat:       1,
+	})
+}
+
+func (r *Review) ChangeLike(c *gin.Context) {
+	// 해당 글 id find
+	// 유저가 좋아요 누른 게시물 리스트에 있으면 -1
+	// 없으면 +1
+
+	id := c.Param("id")
+	_ = id
+
+	req := protocol.ReviewPostReq{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		r.ctl.RespError(c, &req, http.StatusUnprocessableEntity, "ShouldBindJSON", err)
+		return
+	}
+
+	// review post info 에서 likeusers 에서 해당 유저의 id find
+	// 없으면 +1
+	// 있으면 -1
+
+}
+
+func (r *Review) Comment(c *gin.Context) {
+
+}
--- model/community_db.go
+++ model/community_db.go
@@ -2,10 +2,14 @@ package model
 
 import (
 	"community/conf"
+	"community/protocol"
 	"context"
 	"go-common/klay/elog"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
 )
 
 type CommunityDB struct {
@@ -14,6 +18,7 @@ type CommunityDB struct {
 	collectionPostInfo    *mongo.Collection
 	collectionFileInfo    *mongo.Collection
 	collectionCommentInfo *mongo.Collection
+	collectionReviewInfo  *mongo.Collection
 	start                 chan struct{}
 }
 
@@ -38,6 +43,7 @@ func NewCommunityDB(config *conf.Config, root *Repositories) (IRepository, error
 		r.collectionPostInfo = db.Collection("post_info")
 		r.collectionFileInfo = db.Collection("file_info")
 		r.collectionCommentInfo = db.Collection("comment_info")
+		r.collectionReviewInfo = db.Collection("review_info")
 	}
 
 	elog.Trace("load repository : CommunityDB")
@@ -54,4 +60,123 @@ func (p *CommunityDB) Start() error {
 		close(p.start)
 		return
 	}()
+}
+
+func (p *CommunityDB) CreatePost(req protocol.PostWriteReq) error {
+	//result := protocol.PostInfo{}
+	//_, err := p.collectionPostInfo.InsertOne(context.Background(), )
+	return nil
+}
+
+func (p *CommunityDB) GetReviewList(result *[]protocol.ReviewPost) error {
+	filter := bson.M{
+		"stat": 1,
+	}
+	sort := bson.D{{"create_at", 1}}
+	findOptions := options.Find().SetSort(sort)
+
+	if cursor, err := p.collectionReviewInfo.Find(context.Background(), filter, findOptions); err != nil {
+		return err
+	} else {
+		defer cursor.Close(context.Background())
+		cursor.All(context.Background(), result)
+		return nil
+	}
+}
+
+func (p *CommunityDB) GetReviewDetail(id string, result *protocol.ReviewPost) error {
+
+	convertId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.M{
+		"_id":  convertId,
+		"stat": 1,
+	}
+
+	err = p.collectionReviewInfo.FindOne(context.Background(), filter).Decode(&result)
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
+
+}
+
+func (p *CommunityDB) CreateReviewPost(req protocol.ReviewPostReq) error {
+
+	post := protocol.ReviewPost{
+		Id:       primitive.NewObjectID(),
+		Title:    req.Title,
+		UserId:   req.UserId,
+		Content:  req.Content,
+		Likes:    0,
+		CreateAt: time.Now(),
+		UpdateAt: time.Now(),
+		Stat:     1,
+		Grade:    req.Grade,
+	}
+
+	_, err := p.collectionReviewInfo.InsertOne(context.Background(), post)
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
+
+}
+
+func (p *CommunityDB) UpdateReviewPost(id string, req protocol.ReviewPostReq) error {
+	convertId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.M{
+		"_id":  convertId,
+		"stat": 1,
+	}
+	update := bson.M{
+		"$set": bson.M{
+			"title":     req.Title,
+			"content":   req.Content,
+			"update_at": time.Now(),
+			"grade":     req.Grade,
+		},
+	}
+
+	_, err = p.collectionReviewInfo.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
+
+}
+
+func (p *CommunityDB) DeleteReviewPost(id string) error {
+	convertId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.M{
+		"_id":  convertId,
+		"stat": 1,
+	}
+	update := bson.M{
+		"$set": bson.M{
+			"stat":      0,
+			"update_at": time.Now(),
+		},
+	}
+
+	_, err = p.collectionReviewInfo.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
 }
